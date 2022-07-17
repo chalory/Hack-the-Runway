016d8e0c79439e230fc03c8b9aad52069c57a8e8
Brayton-Lordianto <braylord30@gmail.com>
2022-07-17 12:35:50 +0800

    added whatsapp GET endpoint

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+from db import *
+from pprint import pprint
 app = Flask(__name__)
+from get_similar_products import *
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/similar_products/<img_url>")
+def similar_products(img_url):
+    return get_similar_products_info(img_url)
+
+@app.route("/get_gallery/<user>")
+def get_gallery(phone_number):
+    # phone is not used right now
+    return get_wa_table()
+
+
+
+if __name__ == '__main__':
+      app.run(port=5000)
