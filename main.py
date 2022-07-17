c3002f7bce2770f61cbef4d8ab8712e2f76f93d1
chalory <chantalrain.pino@gmail.com>
2022-07-17 11:57:58 +0800

    Added the get_similar_products endpoint

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
-
+from flask import Flask, jsonify
+from db import *
+from pprint import pprint
 app = Flask(__name__)
+from get_similar_products import *
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/similar_products/<img_url>")
+def similar_products(img_url):
+    return get_similar_products_info(img_url)
+
+
+return_similar_products()   
+
+
+
+if __name__ == '__main__':
+      app.run(port=5000)
